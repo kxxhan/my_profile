25331470e558d8d22df1c546854fd2aeefce7633
kxxhan <ofliberty2@gmail.com>
2023-12-15 14:52:14 +0900

    deploy test

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "layout";
 import Main from "pages/Main";
 import Info from "pages/Info";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Main />} />
           <Route path="/info" element={<Info />} />
         </Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
